2b8ee95c73ce8bc808f6623239f690b91d03601b
ntenseobono <ntenseobono@gmail.com>
2024-11-06 15:07:42 -0600

    Http status

--- status_page/status.py
+++ status_page/status.py
@@ -1,28 +1,32 @@
 import flask
-import get_status
+from flask import jsonify
+import get_status 
+from status_param import IP
 
-
-VM1_IP = '52.225.235.130'
-VM2_IP = '65.52.239.81'
 app = flask.Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/') 
 def home():
-    if (get_status.ping_vm_good(VM1_IP)):
-        vm1_status = 'current_status_container'
-    else:
-        vm1_status = 'issue_container'
+    vm1_status = 'current_status_container' if get_status.ping_vm_good(IP.VM1_IP) else 'issue_container'
+    vm2_status = 'current_status_container' if get_status.ping_vm_good(IP.VM2_IP) else 'issue_container'
+    http_status = 'current_status_container' if get_status.update_status() else 'issue_container'
         
-    if (get_status.ping_vm_good(VM2_IP)):
-        vm2_status = 'current_status_container'
-    else:
-        vm2_status = 'issue_container'
-        
-    html_dict = {'vm1_status': vm1_status, 'vm2_status': vm2_status}
-    return flask.render_template('index.html', **html_dict)
+    html_dict = {'vm1_status': vm1_status, 'vm2_status': vm2_status, 'http_status': http_status}
+    return flask.render_template('index.html', **html_dict) 
 
+@app.route('/status', methods=['GET'])
+def get_status_data():
+    vm1_status = 'current_status_container' if get_status.ping_vm_good(IP.VM1_IP) else 'issue_container'
+    vm2_status = 'current_status_container' if get_status.ping_vm_good(IP.VM2_IP) else 'issue_container'
+    http_status = 'current_status_container' if get_status.update_status() else 'issue_container'
 
+    status_data = {
+        'vm1_status': vm1_status,
+        'vm2_status': vm2_status,
+        'http_status': http_status
+    }
     
+    return jsonify(status_data)
 
 if __name__ == '__main__':
     app.run(debug = True)
---
+++ status_page/status_param.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class Https:
+    url_home= "http://onemovechess-web.northcentralus.cloudapp.azure.com/"
+    url_register= "http://onemovechess-web.northcentralus.cloudapp.azure.com/Register"
+    url_login= "http://onemovechess-web.northcentralus.cloudapp.azure.com/login"
+
+@dataclass
+class IP:
+    VM1_IP = '52.225.235.130'
+    VM2_IP = '65.52.239.81'
+
+status_data = {
+    "home": {"status": None, "url": Https.url_home},
+    "register": {"status": None, "url": Https.url_register},
+    "login": {"status": None, "url": Https.url_login}
+}

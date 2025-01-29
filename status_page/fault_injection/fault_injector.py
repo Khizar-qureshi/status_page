29e8ef6f3f6b696f5750931987d392095a33dbee
Sam Lengyel <lengyels@carleton.edu>
2025-01-29 00:13:16 -0600

    added fault injection to drop games table

--- status_page/fault_injection/fault_injector.py
+++ status_page/fault_injection/fault_injector.py
@@ -7,3 +7,6 @@ def kill_api()
 def kill_webui()
     os.system("ssh -i ~/.ssh/VM-Key.pem azureuser@onemovechess-web.northcentralus.cloudapp.azure.com pkill dotnet")
 
+#note: don't use this until we back up the db
+def drop_games_from_db()
+    os.system("ssh -i ~/.ssh/VV-Key.pem azureuser@onemovechess-api.eastus2.cloudapp.azure.com sqlite3 /home/azureuser/.config/OneMoveChess.db 'DROP TABLE games'")

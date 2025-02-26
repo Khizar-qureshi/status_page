ed43623f00998ec0717f284e15c3d8e18329c2d4
SamLengyel <samlengyel2003@outook.com>
2025-02-26 09:04:48 -0600

    added fault_fixer for renaming the database

--- status_page/fault_injection/fault_fixer.py
+++ status_page/fault_injection/fault_fixer.py
@@ -6,5 +6,8 @@ def restart_api():
 def restart_webui():
     os.system('ssh -i ~/.ssh/VM-Key.pem azureuser@onemovechess-web.northcentralus.cloudapp.azure.com "sudo killall dotnet; cd /opt/OneMoveChess; nohup dotnet OneMoveChess.WebUI.dll 1> /var/log/onemovechess.log 2> /var/log/onemovechess.log &"')
 
+def rename_db():
+    os.system('ssh -i ~/.ssh/VM-Key.pem azureuser@onemovechess-api.eastus2.cloudapp.azure.com mv /home/azureuser/.config/*.db /home/azureuser/.config/OneMoveChess.db')
+
 restart_api()
 restart_webui()

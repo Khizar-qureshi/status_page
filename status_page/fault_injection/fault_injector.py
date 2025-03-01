09eece181d425dfc89d1220fbc7dd9e1d9d09df1
SamLengyel <samlengyel2003@outook.com>
2025-02-28 20:38:56 -0600

    changed fault injector to use killall rather than pkill

--- status_page/fault_injection/fault_injector.py
+++ status_page/fault_injection/fault_injector.py
@@ -4,14 +4,14 @@ import time
 
 def kill_api():
     print("Starting to kill api...")
-    os.system('ssh -i ~/.ssh/VM-Key.pem azureuser@onemovechess-api.eastus2.cloudapp.azure.com "sudo pkill dotnet"')
+    os.system('ssh -i ~/.ssh/VM-Key.pem azureuser@onemovechess-api.eastus2.cloudapp.azure.com "sudo killall dotnet"')
     time.sleep(3)
     print("API on VM1 has been killed.")
     time.sleep(10)
     
 def kill_webui():
     print("Starting to kill web ui...")
-    os.system("ssh -i ~/.ssh/VM-Key.pem azureuser@onemovechess-web.northcentralus.cloudapp.azure.com sudo pkill dotnet")
+    os.system("ssh -i ~/.ssh/VM-Key.pem azureuser@onemovechess-web.northcentralus.cloudapp.azure.com sudo killall dotnet")
     time.sleep(3)
     print("Web UI on VM2 has been killed.")
     time.sleep(10)
